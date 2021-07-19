9954058e14033ebb932da9035f582fe1dc579fff
son <son@MacBook-Air-cua-macbook.local>
2021-07-19 10:56:45 +0700

    add home

---
+++ UIDesign/Cell/SliderCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SliderCollectionViewCell.swift
+//  UIDesign
+//
+//  Created by son on 7/19/21.
+//
+
+import UIKit
+
+class SliderCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
